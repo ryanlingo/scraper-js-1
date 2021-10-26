f0a7116f9f352b51d28d15efc44838f79e2c7bbb
Ryan Lingo <ryanlingo@gmail.com>
2021-10-26 07:48:58 -0400

    scraper first iteration

---
+++ index.js
@@ -0,0 +1,15 @@
+const request = require('request-promise');
+const fs = require('fs');
+const cheerio = require('cheerio');
+
+async function main() {
+  const html = await request.get(
+    'https://reactnativetutorial.net/css-selectors/',
+  );
+  fs.writeFileSync('./test.html', html);
+  const $ = await cheerio.load(html);
+  const theText = $('h1').text();
+  console.log(theText);
+}
+
+main();
